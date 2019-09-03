a1efb96418e61103f115a97d91b8b3ced283bea9
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-09-03 12:29:33 +0530

    For Vs While Performance

--- ForVsWhile/Program.cs
+++ ForVsWhile/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics; 
+
 using System.Text;
 
 namespace ForVsWhile
@@ -9,6 +11,31 @@ namespace ForVsWhile
     {
         static void Main(string[] args)
         {
+            int j;
+            Stopwatch st= new Stopwatch();
+            st.Start();
+            for (j = 0; j < 100; j++)
+            {
+
+                j = j * 2;
+
+            }
+            st.Stop();
+            Console.WriteLine(st.ElapsedTicks.ToString());
+            st.Reset();
+
+            st.Start();
+            j = 0;
+            while(j<100)
+
+            {
+
+                j = j * 2;
+                j++;
+
+            }
+            st.Stop();
+            Console.WriteLine(st.ElapsedTicks.ToString());
         }
     }
 }
